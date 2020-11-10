8721b01163df82a68fdfe5ea9b8722b533e10779
Rahul <r.banerji@samsung.com>
2020-11-10 07:12:14 +0000

    First Commit with skeleton code
    Change-Id: I61a0d9607bc8f32c20efb415da399788c6c79c92
    Signed-off-by: Rahul Banerji <r.banerji@samsung.com>

---
+++ setup.py
@@ -0,0 +1,30 @@
+# ==================================================================================
+#
+#       Copyright (c) 2020 Samsung Electronics Co., Ltd. All Rights Reserved.
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#          http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+# ==================================================================================
+
+from setuptools import setup, find_packages
+
+setup(
+    name='hw_python',
+    version='0.0.1',
+    packages=find_packages(),
+    url='https://gerrit.o-ran-sc.org/r/admin/repos/ric-app/hw-python',
+    license='Apache 2.0',
+    install_requires=["ricxappframe>=1.1.1,<2.0.0"],
+    entry_points={"console_scripts": ["run-hw-python.py=src.main:start"]},  # adds a magical entrypoint for Docker
+    data_files=[("", ["LICENSE.txt"])],
+)
+
