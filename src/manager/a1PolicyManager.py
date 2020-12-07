6ba4396e07fb72506801dcc43ea9ed7bcea0f356
RahulBanerji <rahulbanerji96@gmail.com>
2020-12-07 13:42:19 +0530

    Added A1 policy handler, healthcheck handler, sdl handler and alarm
    Issue-Id: [ RICAPP-157 | RICAPP-158 | RICAPP-159 | RICAPP-160 ]
    Change-Id: Icd0363fa6b6cc2d96252fb971d02db73e2c6b612
    Signed-off-by: Rahul Banerji <r.banerji@samsung.com>

---
+++ src/manager/a1PolicyManager.py
@@ -0,0 +1,63 @@
+# ==================================================================================
+#
+#       Copyright (c) 2020 Samsung Electronics Co., Ltd. All Rights Reserved.
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#          http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+# ==================================================================================
+
+import json
+from ricxappframe.xapp_frame import RMRXapp, rmr
+from ..utils.constants import constants
+from .baseManager import baseManager
+
+
+class A1PolicyManager(baseManager):
+
+    def __init__(self, rmr_xapp: RMRXapp):
+        super.__init__(rmr_xapp)
+
+    def startup(self):
+        policy_query = '{"policy_type_id":"' + str(constants.HELLOWORLD_POLICY_ID) + '"}'
+        self._rmr_xapp.rmr_send(policy_query.encode(), constants.A1_POLICY_QUERY)
+        self.logger.info("A1PolicyManager.startup:: Sent A1 policy query = " + policy_query)
+
+    def resp_handler(self, rmr_xapp, summary, sbuf):
+        self._rmr_xapp.rmr_free(sbuf)
+        try:
+            req = json.loads(summary[rmr.RMR_MS_PAYLOAD])  # input should be a json encoded as bytes
+            self.logger.debug("A1PolicyManager.resp_handler:: Handler processing request")
+        except (json.decoder.JSONDecodeError, KeyError):
+            self.logger.error("A1PolicyManager.resp_handler:: Handler failed to parse request: {}".format(req))
+            return
+        if self.verifyPolicy(req):
+            self.logger.info("A1PolicyManager.resp_handler:: Handler processed request: {}".format(req))
+        else:
+            self.logger.error("A1PolicyManager.resp_handler:: Request verification failed: {}".format(req))
+            return
+        self.logger.debug("A1PolicyManager.resp_handler:: Request verification success: {}".format(req))
+
+        resp = self.buildPolicyResp(req)
+        self._rmr_xapp.rmr_send(json.dumps(resp).encode(), constants.A1_POLICY_RESP)
+        self.logger.info("A1PolicyManager.resp_handler:: Response sent: {}".format(resp))
+
+    def verifyPolicy(self, req: dict):
+        for i in ["policy_type_id", "operation", "policy_instance_id"]:
+            if i not in req:
+                return False
+        return True
+
+    def buildPolicyResp(self, req: dict):
+        req["handler_id"] = self._rmr_xapp.config["xapp_name"]
+        del req["operation"]
+        req["status"] = "OK"
+        return req
---
+++ src/manager/sdlAlarmManager.py
@@ -0,0 +1,45 @@
+# ==================================================================================
+#
+#       Copyright (c) 2020 Samsung Electronics Co., Ltd. All Rights Reserved.
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#          http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+# ==================================================================================
+
+from ricxappframe.xapp_frame import RMRXapp, rmr
+from ricxappframe.alarm import alarm
+from .baseManager import baseManager
+
+
+class sdlAlarmManager(baseManager):
+
+    def __init__(self, rmr_xapp: RMRXapp):
+        super.__init__(rmr_xapp)
+        self.alarm_mgr = alarm.AlarmManager(self._rmr_xapp._mrc, "ric-xapp", "hw-python")
+        self.alarm_sdl = None
+
+    def checkSdl(self):
+        if self._rmr_xapp._sdl.healthcheck():
+            # healthy, so clear the alarm if it was raised
+            if self.alarm_sdl:
+                self.logger.info("sdlAlarmManager: clearing alarm")
+                self.alarm_mgr.clear_alarm(self.alarm_sdl)
+                self.alarm_sdl = None
+        else:
+            # not healthy, so (re-)raise the alarm
+            self.logger.info("sdlAlarmManager: connection to SDL is not healthy, raising alarm")
+            if self.alarm_sdl:
+                self.alarm_mgr.reraise_alarm(self.alarm_sdl)
+            else:
+                self.alarm_sdl = self.alarm_mgr.create_alarm(1, alarm.AlarmSeverity.CRITICAL, "SDL failure")
+                self.alarm_mgr.raise_alarm(self.alarm_sdl)
+            self.logger.warning("sdlAlarmManager: dropping request!")
